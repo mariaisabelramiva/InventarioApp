19971f576b7b91ce41c95da685b5786a7588a158
Maria Isabel Ramírez <mariaisabelrv.2901@gmail.com>
2026-05-12 12:28:22 -0500

    Nueva carpeta models y archivo readme

--- InventarioApp/Program.cs
+++ InventarioApp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("======================================");
+Console.WriteLine("|    SISTEMA DE GESTIÓN DE INVENTARIO    |");
+Console.WriteLine("======================================");
+
+Console.WriteLine();
+
+Console.WriteLine($"Version: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+
+Console.WriteLine();
+
+Console.WriteLine("Estructura del proyecto:");
+Console.WriteLine("    InventarioApp/");
+Console.WriteLine("    |-- Program.cs");
+Console.WriteLine("    |-- InventarioApp.csproj");
+Console.WriteLine("    |-- .gitignore");
+Console.WriteLine("    |-- README.md");
+Console.WriteLine("    |-- src/");
+Console.WriteLine("        |-- Models/ (Proxima clase)");
+
+Console.WriteLine("Configuracion .csproj");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos configurados");
+
+Console.WriteLine();
+
+Console.WriteLine("Proximo paso: Checkpoint"); 
